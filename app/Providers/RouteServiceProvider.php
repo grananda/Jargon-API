f4582715c606a02226be5b010063369bc8d65d5f
Julio M. Fernandez <jfernandez74@gmail.com>
2019-03-03 22:51:34 +0100

    Set advance api auth system safe point 2

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -52,8 +52,10 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+            ->namespace($this->namespace)
+            ->group(function () {
+                require base_path('routes/web.php');
+            });
     }
 
     /**
@@ -66,8 +68,10 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(function () {
+                require base_path('routes/api.php');
+            });
     }
 }
