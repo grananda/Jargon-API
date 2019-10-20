22a5377e232fec00ffac41654d4c2673d0926801
Julio Fernandez <jfernandez74@gmail.com>
2019-10-20 18:49:07 +0200

    feature: added code analysis support

--- routes/memos-staff.php
+++ routes/memos-staff.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'staff'], function () {
-    Route::get('/staff/memo', 'Communication\MemoManagementController@index')->name('memos.staff.index');
-    Route::get('/staff/memo/{id}', 'Communication\MemoManagementController@show')->name('memos.staff.show');
-    Route::post('/staff/memo', 'Communication\MemoManagementController@store')->name('memos.staff.store');
-    Route::put('/staff/memo/{id}', 'Communication\MemoManagementController@update')->name('memos.staff.update');
-    Route::delete('/staff/memo/{id}', 'Communication\MemoManagementController@destroy')->name('memos.staff.destroy');
+    Route::get('/staff/memos', 'Communication\MemoManagementController@index')->name('memos.staff.index');
+    Route::get('/staff/memos/{id}', 'Communication\MemoManagementController@show')->name('memos.staff.show');
+    Route::post('/staff/memos', 'Communication\MemoManagementController@store')->name('memos.staff.store');
+    Route::put('/staff/memos/{id}', 'Communication\MemoManagementController@update')->name('memos.staff.update');
+    Route::delete('/staff/memos/{id}', 'Communication\MemoManagementController@destroy')->name('memos.staff.destroy');
 });
